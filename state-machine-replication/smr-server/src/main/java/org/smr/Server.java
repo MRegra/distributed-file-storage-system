cccd2b5da9b587cfe673a012161ac44dce86b14b
MRegra <marregsil97@gmail.com>
2022-12-19 12:03:18 +0100

    Final structure update (hopefully)

--- state-machine-replication/smr-server/src/main/java/org/smr/Server.java
+++ state-machine-replication/smr-server/src/main/java/org/smr/Server.java
@@ -1,6 +1,9 @@
-package com.smr;
+package org.smr;
 
-public class Main {
+/**
+ * @author MRegra on ${DAY}/${MONTH}/${YEAR}
+ */
+public class Server {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
